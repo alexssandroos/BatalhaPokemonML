1691c10257be1da89b3003219b6d974ff3564fc3
Alexssandro Oliveira <alexssandroos@yahoo.com.br>
2019-05-25 14:18:46 -0300

    Update battle.py

--- BatalhaPokemonML/battle.py
+++ BatalhaPokemonML/battle.py
@@ -1,14 +1,8 @@
 import pandas as pd
 import numpy as np
-#import dataset
-pokemon=pd.read_csv('/home/alexssandroos/Público/BatalhaPokemonML/data/pokemons_data.csv',index_col=0)
-combats=pd.read_csv('/home/alexssandroos/Público/BatalhaPokemonML/data/combats.csv')
 
-#display first 10 rows
-pokemon.head(10)
-
-#https://www.eurogamer.net/articles/2018-12-21-pokemon-go-type-chart-effectiveness-weaknesses
-type_vantagens = {
+#utils 
+tipo_vantagens = {
   'Grass': ['Ground', 'Rock', 'Water'], 
   'Fire': ['Bug', 'Steel', 'Grass', 'Ice'], 
   'Water': ['Ground', 'Rock', 'Fire'], 
@@ -28,41 +22,19 @@ type_vantagens = {
   'Steel': ['Rock', 'Ice', 'Fairy'], 
   'Flying' : ['Fighting', 'Bug', 'Grass']}
 
-type_vantagens['Grass'].count('Rock') 
-#replace ids with pokemon names
-cols = ["First_pokemon","Second_pokemon","Winner"]
-new_combat_data=combats[cols].replace(pokemon.Name)
-new_combat_data.head()
-
-
-#Prepare actual Winner column (the actual output for training)    
-combats.Winner[combats.Winner == combats.First_pokemon] = 0
-combats.Winner[combats.Winner == combats.Second_pokemon] = 1
-
+#https://www.eurogamer.net/articles/2018-12-21-pokemon-go-type-chart-effectiveness-weaknesses
+def has_type_advantage(type1, type2):
+  return tipo_vantagens[type1].count(type2)
 
 def inverted_dataframe(df):
   list_first = list(df['First_pokemon'])
   df['First_pokemon'], df['Second_pokemon'] = df['Second_pokemon'],list_first 
   df['Winner'] = (df['Winner']+-1)*-1
+  df['has_type_advantage'] = (df['has_type_advantage']+-1)*-1
   return df
 
-
-combats_invers = combats.copy().pipe(inverted_dataframe)
-
-combats_final = combats.append(combats_invers,\
-     ignore_index=True)
-
-win_rate_First = combats_final.groupby('First_pokemon')['Winner'].sum()\
-  /combats_final.groupby('First_pokemon')['Winner'].count()
-win_rate_Second = combats_final.groupby('Second_pokemon')['Winner'].sum()\
-  /combats_final.groupby('Second_pokemon')['Winner'].count()
-combats_final['win_rate_first'] = combats_final['First_pokemon'].map(win_rate_First)
-combats_final['win_rate_second'] = combats_final['Second_pokemon'].map(win_rate_Second)     
-combats_final['win_rate_battle'] = combats_final['win_rate_first']/combats_final['win_rate_second']
- 
-#Taking diff between pokemon stats and normalizing the data
 def normalization(data_df):
-    stats=["Hit Points","Attack","Defense","Sp. Atk","Sp. Def","Speed","Legendary"]
+    stats=["HP","Attack","Defense","Sp. Atk","Sp. Def","Speed","Legendary"]
     stats_df=pokemon[stats].T.to_dict("list")
     one=data_df.First_pokemon.map(stats_df)
     two=data_df.Second_pokemon.map(stats_df)
@@ -75,12 +47,44 @@ def normalization(data_df):
         new_test[c]=(new_test[c]-description['min'])/(description['max']-description['min'])
     return new_test
 
+def apply_pipeline(df_combats, df_pokemon, type_strong_against):
+  df_combats['first_type1'] = df_combats['First_pokemon'].replace(df_pokemon['Type 1'])
+  df_combats['second_type1'] = df_combats['Second_pokemon'].replace(df_pokemon['Type 1'])
+  df_combats['has_type_advantage'] = df_combats.apply(lambda x: has_type_advantage(x[3], x[4]), axis=1)
+  
+  df_combats.Winner[df_combats.Winner == df_combats.First_pokemon] = 0
+  df_combats.Winner[df_combats.Winner == df_combats.Second_pokemon] = 1
 
-data=normalization(combats_final)
-data = pd.concat([data,combats_final.Winner], axis=1)
-data = pd.concat([data,combats_final.win_rate_battle], axis=1)
-data = data.replace([np.inf, -np.inf], 0)
+  pokemon_legendary_map = {False: 0, True: 1}
+  df_pokemon['Legendary'] = df_pokemon['Legendary'].map(pokemon_legendary_map)
 
+  combats_invers = df_combats.copy().pipe(inverted_dataframe)
+  combats_final = df_combats.append(combats_invers,\
+     ignore_index=True)
+
+  win_rate_First = combats_final.groupby('First_pokemon')['Winner'].sum()\
+  /combats_final.groupby('First_pokemon')['Winner'].count()
+  win_rate_Second = combats_final.groupby('Second_pokemon')['Winner'].sum()\
+  /combats_final.groupby('Second_pokemon')['Winner'].count()
+  combats_final['win_rate_first'] = combats_final['First_pokemon'].map(win_rate_First)
+  combats_final['win_rate_second'] = combats_final['Second_pokemon'].map(win_rate_Second)     
+  combats_final['win_rate_battle'] = combats_final['win_rate_first']/combats_final['win_rate_second']
+
+  data=normalization(combats_final)
+  data = pd.concat([data,combats_final.Winner], axis=1)
+  data = pd.concat([data,combats_final.win_rate_battle], axis=1)
+  data = pd.concat([data,combats_final.has_type_advantage], axis=1)
+  data = data.replace([np.inf, -np.inf], 0)
+
+  return data
+
+
+
+#import dataset
+pokemon=pd.read_csv('/home/alexssandroos/Público/BatalhaPokemonML/data/pokemon.csv',index_col=0)
+combats=pd.read_csv('/home/alexssandroos/Público/BatalhaPokemonML/data/combats.csv')
+
+data = apply_pipeline(combats, pokemon, tipo_vantagens)  
 x_label=data.drop("Winner",axis=1) #inputs
 y_label=data["Winner"] #outputs
 
@@ -104,18 +108,19 @@ print('Accuracy of ', accuracy_score(pred, y_test)*100)
 
 #importing test dataset 
 
-test_data=pd.read_csv('./data/tests.csv') 
+data_valid=pd.read_csv('./data/tests.csv') 
+data_valid['Winner'] = 0
 
 
 #replace ids with name
-new_test_data=test_data[["First_pokemon","Second_pokemon"]].replace(pokemon.Name)
-new_test_data.head()
+new_test_data=data_valid[["First_pokemon","Second_pokemon"]].replace(pokemon.Name)
+new_test_data['Winner'] = 0
 
 #normalizing test data
-final_data=normalization(test_data)
-pred=model.predict(final_data)#final predictions
-test_data["Winner"]=[test_data["First_pokemon"][i] if pred[i]==0 else test_data["Second_pokemon"][i] for i in range(len(pred))]
+final_data=apply_pipeline(data_valid, pokemon, tipo_vantagens)  
+pred=model.predict(final_data.drop(columns=['Winner'))#final predictions
+data_valid["Winner"]=[data_valid["First_pokemon"][i] if pred[i]==0 else data_valid["Second_pokemon"][i] for i in range(len(pred))]
 
-combats_name = test_data[cols].replace(pokemon.Name)
+combats_name = data_valid[cols].replace(pokemon.Name)
 combats_name[63:64]
 
